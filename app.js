66d9813db9ff664100156eed7e60df050c946f3f
Turan Sultan <turan.sultann@gmail.com>
2020-05-23 22:17:07 +0600

    commit

--- app.js
+++ app.js
@@ -12,32 +12,76 @@ input.addEventListener('keypress', function(e){
 btn.addEventListener('click', addTask);
 listContainer.addEventListener('click', deleteTask);
 listContainer.addEventListener('click', crossOut);
-//functions
-function addTask(){
-    if (input.value == 0){
-        
+
+// Setting to localStorage
+if (localStorage.hasOwnProperty('tasks')){
+    if (input.value != ''){
+        let taskList = JSON.parse(localStorage.getItem('tasks'));
+        taskList.push(input.value);
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+    }
+    
+}
+else{
+    if (input.value != ''){
+        let taskList = [input.value];
+        localStorage.setItem('tasks', JSON.stringify(taskList));
     }
     else{
-        var box = document.createElement('div');
-        box.classList.add('list-box');
-        listContainer.appendChild(box);
-        var check = document.createElement('input');
-        check.setAttribute('type', 'checkbox');
-        box.appendChild(check);
-        var li = document.createElement('li');
-        li.textContent = input.value;
-        box.appendChild(li);
-        var img = document.createElement('div');
-        img.classList.add('image-cont');
-        img.innerHTML = '<img src="https://img.icons8.com/plasticine/100/000000/filled-trash.png"/>';
-        box.appendChild(img);
-        input.value = "";
+        
     }
+    
+}
+let taskList = JSON.parse(localStorage.getItem('tasks'));
+taskList.forEach(element => {
+    let box = document.createElement('div');
+    box.classList.add('list-box');
+    let check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    let li = document.createElement('li');
+    li.textContent = element;
+    let img = document.createElement('div');
+    img.classList.add('image-cont');
+    box.appendChild(check);
+    box.appendChild(li);
+    listContainer.appendChild(box);
+    img.innerHTML = '<img src="https://img.icons8.com/plasticine/100/000000/filled-trash.png"/>';
+    box.appendChild(img);  
+    console.log(element)
+});
+//functions
+function addTask(){
+    if (input.value != ''){
+        
+        let taskList = JSON.parse(localStorage.getItem('tasks'));
+        taskList.push(input.value);
+        localStorage.setItem('tasks', JSON.stringify(taskList))
+        
+        let box = document.createElement('div');
+        box.classList.add('list-box');
+        let check = document.createElement('input');
+        check.setAttribute('type', 'checkbox');
+        let li = document.createElement('li');
+        li.textContent = input.value;
+        let img = document.createElement('div');
+        img.dataset.id = input.value;
+
+        img.classList.add('image-cont');
+        box.appendChild(check);
+        box.appendChild(li);
+        listContainer.appendChild(box);
+        img.innerHTML = '<img src="https://img.icons8.com/plasticine/100/000000/filled-trash.png"/>';
+        box.appendChild(img);  
+    }
+    input.value = '';
 }
 
 function deleteTask(trash){
     if (trash.target.parentElement.classList.contains("image-cont")){
-        trash.target.parentElement.parentElement.remove()
+        trash.target.parentElement.parentElement.remove();
+        let taskList = JSON.parse(localStorage.getItem('tasks'));
+        taskList = taskList.filter((e)=>{ e != trash.target.parentElement.dataset.id });
+        localStorage.setItem('tasks', JSON.stringify(taskList))
     }
 }
 
