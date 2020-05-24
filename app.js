48a829ce24d6991c8db892a23a06adb5034bd40d
Turan Sultan <turan.sultann@gmail.com>
2020-05-24 12:15:14 +0600

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // constants
 const input = document.querySelector('input');
 const listContainer = document.querySelector('.list-container');
-const btn = document.querySelector('button');
+const addBtn = document.querySelector('button');
 
 // event listeners
 input.addEventListener('keypress', function(e){
@@ -9,47 +9,51 @@ input.addEventListener('keypress', function(e){
         addTask()
     }
 })
-btn.addEventListener('click', addTask);
+addBtn.addEventListener('click', addTask);
 listContainer.addEventListener('click', deleteTask);
 listContainer.addEventListener('click', crossOut);
+window.onload = setToStarage();
 
 // Setting to localStorage
-if (localStorage.hasOwnProperty('tasks')){
-    if (input.value != ''){
-        let taskList = JSON.parse(localStorage.getItem('tasks'));
-        taskList.push(input.value);
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-    }
-    
-}
-else{
-    if (input.value != ''){
-        let taskList = [input.value];
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-    }
-    else{
+function setToStarage(){
+    if (localStorage.hasOwnProperty('tasks')){
+        if (input.value != ''){
+            let taskList = JSON.parse(localStorage.getItem('tasks'));
+            taskList.push(input.value);
+            localStorage.setItem('tasks', JSON.stringify(taskList));
+        }
         
     }
+    else{
+        if (input.value != ''){
+            let taskList = [input.value];
+            localStorage.setItem('tasks', JSON.stringify(taskList));
+        }
+        else{
+            
+        }
+        
+    }
+    let taskList = JSON.parse(localStorage.getItem('tasks'));
+    taskList.forEach(element => {
+        let box = document.createElement('div');
+        box.classList.add('list-box');
+        let check = document.createElement('input');
+        check.setAttribute('type', 'checkbox');
+        let li = document.createElement('li');
+        li.textContent = element;
+        let img = document.createElement('img');
+        img.classList.add('image');
+        img.dataset.id = element;
+        box.appendChild(check);
+        box.appendChild(li);
+        listContainer.appendChild(box);
+        img.src ="https://img.icons8.com/plasticine/100/000000/filled-trash.png";
+        box.appendChild(img);  
     
+    });
 }
-let taskList = JSON.parse(localStorage.getItem('tasks'));
-taskList.forEach(element => {
-    let box = document.createElement('div');
-    box.classList.add('list-box');
-    let check = document.createElement('input');
-    check.setAttribute('type', 'checkbox');
-    let li = document.createElement('li');
-    li.textContent = element;
-    let img = document.createElement('img');
-    img.classList.add('image');
-    img.dataset.id = element;
-    box.appendChild(check);
-    box.appendChild(li);
-    listContainer.appendChild(box);
-    img.src ="https://img.icons8.com/plasticine/100/000000/filled-trash.png";
-    box.appendChild(img);  
 
-});
 //functions
 function addTask(){
     if (input.value != ''){  
